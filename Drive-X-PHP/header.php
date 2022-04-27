23d216a7a4b3c8c7e0fced6a4914358631e4b3bd
Florian <92161528+peflor@users.noreply.github.com>
2022-04-27 12:27:37 +0200

    Index & footer ok

---
+++ Drive-X-PHP/header.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+
+        <a href="index.html" class="navbar-brand text-danger">Drive-X</a>
+
+        <ul class="navbar-nav">
+            <li class="nav-item" >
+                <a href="cars.html" class="nav-link">Voitures</a>
+            </li>
+            <li class="nav-item" >
+                <a href="drivers.html" class="nav-link">Pilotes</a>
+            </li>
+        </ul>
+
+    </nav>
+
+</header>
---
+++ Drive-X-PHP/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Drive-X</title>
+</head>
+
+<body>
+    <?php require_once "header.php"?>
+
+
+
+    <?php require_once "footer.php"?>
+</body>
+
+</html>
