a9744e8583fc03da215b7d849a47bcab26e1b5ac
Florian <92161528+peflor@users.noreply.github.com>
2022-04-27 13:19:44 +0200

    Update repoData.php

--- Drive-X-PHP/repoData.php
+++ Drive-X-PHP/repoData.php
@@ -59,48 +59,48 @@ $drivers = [
     ],
   ];
 
-  cars: Car[] = [
+  $cars = [
     [
-      name: "pagani huayra",
+      "name" => "pagani huayra",
       "pays" => "italie",
       "coverImage" =>"./assets/img/cars/pagani.jpg",
-      power: 765,
-      perf: 3.2
+      "power" => 765,
+      "perf" => 3.2
     ],
     [
-      name: "koenigsegg agera rs",
+      "name" => "koenigsegg agera rs",
       "pays" => "suède",
       "coverImage" =>"./assets/img/cars/koenigsegg.jpg",
-      power: 1383,
-      perf: 2.6
+      "power" => 1383,
+      "perf" => 2.6
 
     ],
     [
-      name: "zenvo tsr s",
+      "name" => "zenvo tsr s",
       "pays" => "danemark",
       "coverImage" =>"./assets/img/cars/zenvo.jpg",
-      power: 1200,
-      perf: 2.8
+      "power" => 1200,
+      "perf" => 2.8
     ],
     [
-      name: "bugatti chiron",
+      "name" => "bugatti chiron",
       "pays" => "france",
       "coverImage" =>"./assets/img/cars/bugatti.jpg",
-      power: 1500,
-      perf: 2.4
+      "power" => 1500,
+      "perf" => 2.4
     ],
     [
-      name: "mercedes amg one",
+      "name" => "mercedes amg one",
       "pays" => "allemagne",
       "coverImage" =>"./assets/img/cars/mercedes.jpg",
-      power: 1000,
-      perf: 2.2
+      "power" => 1000,
+      "perf" => 2.2
     ],
     [
-      name: "ferrari sf90",
+      "name" => "ferrari sf90",
       "pays" => "italie",
       "coverImage" =>"./assets/img/cars/ferrari.jpg",
-      power: 1000,
-      perf: 2.5
+      "power" => 1000,
+      "perf" => 2.5
     ]
   ];
