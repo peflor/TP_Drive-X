48142c9208631e25bcc9468c916d28fd9cb4b43b
Florian <92161528+peflor@users.noreply.github.com>
2022-04-27 13:52:31 +0200

    fichier cars et drivers début

--- Drive-X-PHP/partial/header.php
+++ Drive-X-PHP/partial/header.php
@@ -1,14 +1,14 @@
 <header>
     <nav class="navbar navbar-expand navbar-dark bg-dark">
 
-        <a href="index.html" class="navbar-brand text-danger">Drive-X</a>
+        <a href="index.php" class="navbar-brand text-danger">Drive-X</a>
 
         <ul class="navbar-nav">
             <li class="nav-item" >
-                <a href="cars.html" class="nav-link">Voitures</a>
+                <a href="cars.php" class="nav-link">Voitures</a>
             </li>
             <li class="nav-item" >
-                <a href="drivers.html" class="nav-link">Pilotes</a>
+                <a href="drivers.php" class="nav-link">Pilotes</a>
             </li>
         </ul>
 
